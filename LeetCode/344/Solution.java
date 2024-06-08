a4fff27b04aa6241aecef39c47aca118c8a8c5f9
manrasal <manishrasal2000@gmail.com>
2024-06-08 15:42:06 +0530

    Reverse String

--- LeetCode/344/Solution.java
+++ LeetCode/344/Solution.java
@@ -1,20 +1,15 @@
 /**
- * Problem: Longest Common Prefix
- * Description: Write a function to find the longest common prefix string amongst an array of strings.
- *
- * If there is no common prefix, return an empty string "".
+ * Problem: Reverse String
  */
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        StringBuilder ans = new StringBuilder();
-        for(int i = 0; i < strs[0].length(); i++) {
-            for(int j = 1; j < strs.length; j++) {
-                if(i >= strs[j].length() || strs[j].charAt(i) != strs[0].charAt(i)) {
-                    return ans.toString();
-                }
-            }
-            ans.append(strs[0].charAt(i));
+    public void reverseString(char[] s) {
+        int i = 0, j = s.length - 1;
+        while(i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
         }
-        return ans.toString();
     }
 }
