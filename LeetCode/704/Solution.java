4c8fff6b48cb67017826cf0c51c89fe69cfb2444
manrasal <manishrasal2000@gmail.com>
2024-06-13 21:06:53 +0530

    Binary Search

---
+++ LeetCode/704/Solution.java
@@ -0,0 +1,18 @@
+/**
+ * Problem: Binary Search
+ * Description: Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+ *
+ * You must write an algorithm with O(log n) runtime complexity.
+ */
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while(l <= r) {
+            int mid = l + (r - l)  / 2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target) r = mid - 1;
+            else l = mid + 1;
+        }
+        return -1;
+    }
+}
