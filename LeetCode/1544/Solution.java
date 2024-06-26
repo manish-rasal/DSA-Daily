1439257c95f4d3c2c33072272c55f6133840b37d
manrasal <manishrasal2000@gmail.com>
2024-06-26 16:03:06 +0530

    Make The String Great

---
+++ LeetCode/1544/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * Problem: Make The String Great
+ * Description: Given a string s of lower and upper case English letters.
+ *
+ * A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
+ *
+ * 0 <= i <= s.length - 2
+ * s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
+ * To make the string good, you can choose two adjacent characters that make the string bad and remove them. You can keep doing this until the string becomes good.
+ *
+ * Return the string after making it good. The answer is guaranteed to be unique under the given constraints.
+ *
+ * Notice that an empty string is also good.
+ */
+class Solution {
+    public String makeGood(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(char c: s.toCharArray()) {
+            if(!st.isEmpty() && Math.abs(st.peek() - c) == 32) {
+                st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for(Character ch: st) {
+            result.append(ch);
+        }
+        return result.toString();
+    }
+}
