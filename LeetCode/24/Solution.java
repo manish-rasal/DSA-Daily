5374e229673b33130368e70204aef7f3a0776443
manrasal <manishrasal2000@gmail.com>
2024-07-05 22:36:21 +0530

    Swap Nodes in Pairs

---
+++ LeetCode/24/Solution.java
@@ -0,0 +1,37 @@
+/**
+ * Problem: Swap Nodes in Pairs
+ * Description: Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode prevNode = dummy;
+
+        while ((head != null) && (head.next != null)) {
+
+            ListNode firstNode = head;
+            ListNode secondNode = head.next;
+
+            prevNode.next = secondNode;
+            firstNode.next = secondNode.next;
+            secondNode.next = firstNode;
+
+            prevNode = firstNode;
+            head = firstNode.next;
+        }
+
+        return dummy.next;
+    }
+}
