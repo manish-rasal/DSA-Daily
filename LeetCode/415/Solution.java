86241d64e1d2ec8afdfe6b6d350e82cdafbf55b8
manrasal <manishrasal2000@gmail.com>
2024-07-06 10:10:09 +0530

    Add Strings

---
+++ LeetCode/415/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Problem: Add Strings
+ * Description: Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+ *
+ * You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
+ */
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder result = new StringBuilder();
+
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int n1 = (i >= 0) ? num1.charAt(i) - '0' : 0;
+            int n2 = (j >= 0) ? num2.charAt(j) - '0' : 0;
+
+            int sum = n1 + n2 + carry;
+            carry = sum / 10;
+            result.append(sum % 10);
+
+            i--;
+            j--;
+        }
+
+        return result.reverse().toString();
+    }
+}
