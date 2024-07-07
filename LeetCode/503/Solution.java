0d2602b5cb3dc86478ab07e040bb57a50bd4ffd3
manrasal <manishrasal2000@gmail.com>
2024-07-07 15:40:32 +0530

    Next Greater Element II

---
+++ LeetCode/503/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * Problem: Next Greater Element II
+ * Description: Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), return the next greater number for every element in nums.
+ *
+ * The next greater number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, return -1 for this number.
+ */
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        // Initialize the result array with -1
+        for (int i = 0; i < n; i++) {
+            result[i] = -1;
+        }
+
+        // Traverse the array twice
+        for (int i = 0; i < 2 * n; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i % n]) {
+                result[stack.pop()] = nums[i % n];
+            }
+            if (i < n) {
+                stack.push(i);
+            }
+        }
+
+        return result;
+    }
+}
